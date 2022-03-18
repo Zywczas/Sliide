fd941f9af0e4d9a5d507cbae9241039af42c3596
Piotr Zywczewski <piotr.zywczewski@intive.com>
2022-03-18 15:11:16 +0100

    added 1 todo

--- networkstore/src/main/java/com/zywczas/networkstore/utils/HelperFunctions.kt
+++ networkstore/src/main/java/com/zywczas/networkstore/utils/HelperFunctions.kt
@@ -1,3 +1,3 @@
 package com.zywczas.networkstore.utils
 
-internal fun getBearer(): String = "Bearer 1ca1a7845e304a7395181b6f44f6e675e3b28e229e1ea2bf3095c8923ad484c3" //todo hide it somewhere
+internal fun getBearer(): String = "Bearer 1ca1a7845e304a7395181b6f44f6e675e3b28e229e1ea2bf3095c8923ad484c3" //todo hide it encrypted file
