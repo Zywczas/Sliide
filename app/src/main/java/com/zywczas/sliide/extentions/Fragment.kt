04caa7dc4a47410d309e8e9b688ce594be1af445
Piotr Zywczewski <piotr.zywczewski@intive.com>
2022-03-18 14:54:07 +0100

    improved item divider

--- app/src/main/java/com/zywczas/sliide/extentions/Fragment.kt
+++ app/src/main/java/com/zywczas/sliide/extentions/Fragment.kt
@@ -4,4 +4,4 @@ import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 
-fun Fragment.showSnackbar(@StringRes msg: Int) = Snackbar.make(requireView(), msg, Snackbar.LENGTH_LONG).show() //todo change theme of snackbar
+fun Fragment.showSnackbar(@StringRes msg: Int) = Snackbar.make(requireView(), msg, Snackbar.LENGTH_LONG).show()
