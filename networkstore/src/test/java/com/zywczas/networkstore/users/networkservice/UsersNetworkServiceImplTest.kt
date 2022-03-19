98249724157f5cb2ccb5df0f3ee16a23c9616550
Piotr Zywczewski <piotr.zywczewski@intive.com>
2022-03-19 11:37:48 +0100

    UsersNetworkServiceImplTest finished

--- networkstore/src/test/java/com/zywczas/networkstore/users/networkservice/UsersNetworkServiceImplTest.kt
+++ networkstore/src/test/java/com/zywczas/networkstore/users/networkservice/UsersNetworkServiceImplTest.kt
@@ -20,7 +20,7 @@ import org.junit.Test
 import retrofit2.Response
 
 @ExperimentalCoroutinesApi
-internal class UsersNetworkServiceImplTest {
+class UsersNetworkServiceImplTest {
 
     @get:Rule
     val coroutineTest = TestCoroutineRule()
@@ -52,6 +52,99 @@ internal class UsersNetworkServiceImplTest {
         val actual = tested.getUsersLastPage().errorMessage
 
         verify(usersApi).getUsers(page = 1)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun getUsersLastPage_whenFirstResponseIsError_shouldGetError() = coroutineTest.runBlockingTest {
+        val expected = R.string.cannot_download_users
+        whenever(usersApi.getUsers(any())).thenReturn(Response.success(222, usersResponseMocks.response1))
+
+        val actual = tested.getUsersLastPage().errorMessage
+
+        verify(usersApi).getUsers(page = 1)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun getUsersLastPage_whenLastPageResponseIsError_shouldGetError() = coroutineTest.runBlockingTest {
+        val expected = R.string.cannot_download_users
+        whenever(usersApi.getUsers(any()))
+            .thenReturn(Response.success(200, usersResponseMocks.response1))
+            .thenReturn(Response.success(222, usersResponseMocks.response1))
+
+        val actual = tested.getUsersLastPage().errorMessage
+
+        verify(usersApi).getUsers(page = 1)
+        verify(usersApi).getUsers(page = 23)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun deleteUser_shouldDelete() = coroutineTest.runBlockingTest {
+        val expected = R.string.deleted_user
+        whenever(usersApi.deleteUser(any(), any())).thenReturn(Response.success(204, Unit))
+
+        val actual = tested.deleteUser(2L).data
+
+        verify(usersApi).deleteUser(bearer = any(), userId = eq(2L))
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun deleteUser_shouldCatchException() = coroutineTest.runBlockingTest {
+        val expected = R.string.cannot_delete_user
+        whenever(usersApi.deleteUser(any(), any())).thenThrow(Exception::class.java)
+
+        val actual = tested.deleteUser(2L).errorMessage
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun deleteUser_whenErrorOccurred_shouldGetError() = coroutineTest.runBlockingTest {
+        val expected = R.string.cannot_delete_user
+        whenever(usersApi.deleteUser(any(), any())).thenReturn(Response.success(222, Unit))
+
+        val actual = tested.deleteUser(2L).errorMessage
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun createUser_shouldCreate() = coroutineTest.runBlockingTest {
+        val expected = R.string.created_user
+        whenever(usersApi.createUser(any(), any(), any(), any(), any())).thenReturn(Response.success(201, Unit))
+
+        val actual = tested.createUser("myName", "myEmail").data
+
+        verify(usersApi).createUser(
+            bearer = any(),
+            name = eq("myName"),
+            email = eq("myEmail"),
+            gender = eq("male"),
+            status = eq("active")
+        )
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun createUser_shouldCatchException() = coroutineTest.runBlockingTest {
+        val expected = R.string.cannot_create_user
+        whenever(usersApi.createUser(any(), any(), any(), any(), any())).thenThrow(Exception::class.java)
+
+        val actual = tested.createUser("myName", "myEmail").errorMessage
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun createUser_whenErrorOccurred_shouldGetError() = coroutineTest.runBlockingTest {
+        val expected = R.string.cannot_create_user
+        whenever(usersApi.createUser(any(), any(), any(), any(), any())).thenReturn(Response.success(222, Unit))
+
+        val actual = tested.createUser("myName", "myEmail").errorMessage
+
         assertThat(actual).isEqualTo(expected)
     }
 
