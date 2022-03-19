7398bd44f2602042d862d7b2cdbe135f2cc11950
Piotr Zywczewski <piotr.zywczewski@intive.com>
2022-03-19 17:49:46 +0100

    a comment for reviewers

--- networkstore/src/main/java/com/zywczas/networkstore/utils/HelperFunctions.kt
+++ networkstore/src/main/java/com/zywczas/networkstore/utils/HelperFunctions.kt
@@ -1,3 +1,5 @@
 package com.zywczas.networkstore.utils
-
+//todo comment for reviewers:
+//I have encrypted the file with the 'API_KEY' in utils/Constants.kt.gpg.
+//To use the app it's enough to replace `$API_KEY` with your own key or use an .apk file from my Github repository
 internal fun getBearer(): String = "Bearer $API_KEY"
